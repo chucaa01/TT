065761d780cba9affe17daa738ea741cac79f34c
Chuc4x <jbuendiah1900@alumno.ipn.mx>
2025-05-24 15:28:28 -0600

    Fix: add Export debian

--- startup.sh
+++ startup.sh
@@ -4,29 +4,9 @@
 export DEBIAN_FRONTEND=noninteractive
 
 # Instalar librerías del sistema necesarias para OpenCV, torch, matplotlib, etc.
-apt update && apt install -y \
-    libglib2.0-0 \
-    libgl1 \
-    libsm6 \
-    libxrender1 \
-    libxext6 \
-    libgtk2.0-dev \
-    libgtk-3-dev \
-    ffmpeg \
-    libatlas-base-dev \
-    libjpeg-dev \
-    zlib1g-dev \
-    libpng-dev \
-    libavcodec-dev \
-    libavformat-dev \
-    libswscale-dev \
-    libtbb2 \
-    libtbb-dev \
-    libdc1394-22 \
-    libdc1394-22-dev
-
+apt update && apt install -y libgl1 libglib2.0-0 libsm6 libxrender1 libxext6 libgtk2.0-dev libgtk-3-dev ffmpeg libatlas-base-dev libjpeg-dev zlib1g-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libtbb2 libtbb-dev libdc1394-25 libdc1394-dev
 # Ir al directorio donde está el manage.py y el wsgi.py
 cd backend
 
 # Ejecutar gunicorn apuntando a backend.wsgi
-gunicorn --workers=2 backend.wsgi:application --bind=0.0.0.0:8000 --timeout 600
+
